bfb491aba6480d9b8b3469591d9c2610cee71c35
Daniel Cox <tdanielcox@gmail.com>
2017-07-03 12:38:43 -0600

    move id in Refunds

--- src/Bluesnap/Refund.php
+++ src/Bluesnap/Refund.php
@@ -10,6 +10,7 @@ class Refund
     public static function update($id, $query_params = null)
     {
         return Adapter::update('Refund', $id, null, [
+            'id_in_url' => false,
             'query_params' => $query_params
         ]);
     }
